aca2f5e8a7d361c522183e0aa006514c11475003
Max Zerzouri <maxdamantus@gmail.com>
2014-08-21 14:47:29 +1200

    Pretty much all features of the rec renderer, could do with fine tuning of positions.

--- lgr.js
+++ lgr.js
@@ -1,5 +1,5 @@
 define([], function(){
-	var imgs = ["bike", "ground", "head", "sky", "susp1", "susp2", "wheel"];
+	var imgs = ["bike", "ground", "head", "sky", "susp1", "susp2", "wheel", "qfood1", "qexit", "q1body", "q1forarm", "q1leg", "q1thigh", "q1up_arm", "myshirt"];
 
 	function loading(canv){
 		canv.fillStyle = "#00ff00";
@@ -7,7 +7,7 @@ define([], function(){
 	}
 
 	return function(path){
-		var r = {};
+		var r = { _ident: {} };
 		imgs.forEach(function(i){
 			var img = new Image();
 			img.src = path + "/" + i + ".png";
@@ -19,10 +19,24 @@ define([], function(){
 					canv.fillStyle = canv.createPattern(img, "repeat");
 					canv.fillRect(0, 0, w, h);
 				};
+				r[i].frame = function(canv, num, of){
+					canv.save();
+					canv.beginPath();
+					canv.moveTo(0, 0);
+					canv.lineTo(1, 0);
+					canv.lineTo(1, 1);
+					canv.lineTo(0, 1);
+					canv.clip();
+					canv.drawImage(img, -num, 0, of, 1);
+					canv.restore();
+				};
+
+				r._ident = {};
 			};
 			r[i] = {
 				draw: loading,
-				repeat: loading
+				repeat: loading,
+				frame: loading
 			};
 		});
 		return r;
--- recRender.js
+++ recRender.js
@@ -3,7 +3,6 @@ define([], function(){
 		return Math.sqrt(a*a + b*b);
 	}
 
-
 	function skewimage(canv, img, bx, by, br, ih, x1, y1, x2, y2){
 		var o = x2-x1, a = y2-y1;
 		canv.save();
@@ -28,13 +27,40 @@ define([], function(){
 			return o;
 		}();
 
+		var volts = function(){
+			var ec = reader.eventCount();
+			var o = [];
+			for(var e = 0; e < ec; e++)
+				reader.event(e, function(time, a, b){
+					if(a & 255 == 255)
+						switch(a >> 8){
+							case 1791: // right volt
+								o.push([Math.floor(time/.01455976568094950714), true]);
+								break;
+							case 2047: // left volt
+								o.push([Math.floor(time/.01455976568094950714), false]);
+								break;
+						}
+				});
+				return o;
+		}();
+
 		function lastTurn(frame){
 			for(var x = 0; x < turnFrames.length; x++)
 				if(turnFrames[x] > frame)
-					return x? turnFrames[x - 1] : -1;
+					break;
+			return x? turnFrames[x - 1] : -1;
+		}
+
+		function lastVolt(frame){
+			for(var x = 0; x < volts.length; x++)
+				if(volts[x][0] > frame)
+					break;
+			return x? volts[x - 1] : null;
 		}
 
 		console.log(turnFrames);
+		console.log(volts);
 		// (x, y): top left in Elma coordinates
 		function draw(canv, lgr, frame, x, y, scale){
 			canv.save();
@@ -70,24 +96,6 @@ define([], function(){
 				lgr.wheel.draw(canv);
 			canv.restore();
 
-/*			canv.save(); // head
-				canv.translate(headX/400, -headY/400);
-				canv.rotate(-bikeR);
-				canv.scale(23/48, 23/48);
-
-				// 329 → 332
-
-
-				canv.save();
-
-					canv.translate(-0.5, -0.5);
-					lgr.head.draw(canv);
-				canv.restore();
-
-			canv.restore();
-			*/
-
-
 			canv.save();
 				canv.rotate(-bikeR);
 				if(turn)
@@ -104,14 +112,14 @@ define([], function(){
 					wy = turn? -rightY : -leftY;
 					a = Math.atan2(wy, (turn? -1 : 1) * wx) + (turn? -1 : 1) * bikeR;
 					r = hypot(wx, wy);
-					skewimage(canv, lgr.susp1, 5, 0.5, 5, 7, -20, -17, 48*r * Math.cos(a), 48*r * Math.sin(a));
+					skewimage(canv, lgr.susp1, 5, 0.5, 5, 6.5, -20, -17, 48*r * Math.cos(a), 48*r * Math.sin(a));
 
 					// rear suspension
 					wx = turn? leftX : rightX;
 					wy = turn? -leftY : -rightY;
 					a = Math.atan2(wy, (turn? -1 : 1) * wx) + (turn? -1 : 1) * bikeR;
 					r = hypot(wx, wy);
-					skewimage(canv, lgr.susp2, 5, 0.5, 5, 7, 48*r*Math.cos(a), 48*r*Math.sin(a), 10, 20);
+					skewimage(canv, lgr.susp2, 5, 0.5, 5, 6.5, 48*r*Math.cos(a), 48*r*Math.sin(a), 10, 20);
 				canv.restore();
 
 				canv.save();
@@ -122,46 +130,93 @@ define([], function(){
 					lgr.bike.draw(canv);
 				canv.restore();
 
-				canv.save(); // head
+				canv.save(); // kuski
 					r = hypot(headX, headY);
 					a = Math.atan2(-headY, turn? -headX : headX) + (turn? -bikeR : bikeR);
 					wx = r*Math.cos(a);
 					wy = r*Math.sin(a);
 					canv.translate(wx, wy);
-					canv.beginPath();
-					canv.moveTo(-1, 0);
-					canv.lineTo(1, 0);
-					canv.moveTo(0, -1);
-					canv.lineTo(0, 1);
-					canv.strokeStyle = "#ff0000";
-					canv.lineWidth = 1/50;
-					canv.stroke();
 
+					canv.save(); // head
+						canv.translate(-17/48, -42/48);
+						canv.scale(23/48, 23/48);
+						lgr.head.draw(canv);
+					canv.restore();
 
-				canv.translate(-17/48, -42/48);
-				canv.scale(23/48, 23/48);
-//				canv.translate(-0.5, 0);
-				lgr.head.draw(canv);
+					var bumx = 20/48, bumy = 0;
+					var pedalx = -wx + 4/48, pedaly = -wy + 23/48;
+					var bum2pedal = hypot(pedalx - bumx, pedaly - bumy);
+					var upper = 25/48; // length of
+					var lower = 25/48; // ..
 
+					var prod =
+						(bum2pedal + upper + lower)*
+						(bum2pedal - upper + lower)*
+						(bum2pedal + upper - lower)*
+						(-bum2pedal + upper + lower);
+					var b2pangle = Math.atan2(pedaly - bumy, pedalx - bumx);
+					var jointangle = 0;
+					if(prod >= 0){
+						// law of sines
+						var circumr = bum2pedal*upper*lower/Math.sqrt(prod);
+						jointangle = Math.asin(lower/(2*circumr));
+					}else
+						upper = upper/(upper + lower)*bum2pedal;
+						
+					var jointx = bumx + upper*Math.cos(b2pangle + jointangle);
+					var jointy = bumy + upper*Math.sin(b2pangle + jointangle);
 
+					// leg—a bit repetitive
+					skewimage(canv, lgr.q1thigh, 2/48, 0.5, 6/48, 0.25, jointx, jointy, bumx, bumy);
+					skewimage(canv, lgr.q1leg, 0, 0.5, 3/48, 0.4, pedalx, pedaly, jointx, jointy);
+
+					canv.save(); // torso
+						canv.translate(15/48, 10/48);
+						canv.rotate(Math.PI + Math.PI/5);
+						canv.scale(32/48, 21/48);
+						lgr.q1body.draw(canv);
+					canv.restore();
+
+					var shoulderx = -1/48, shouldery = -15/48;
+					var handx = -wx - 20/48, handy = -wy - 17/48;
+
+					var lower = 17/48, upper = 17/48;
+					var shoulder2hand = hypot(handx - shoulderx, handy - shouldery);
+
+					var lv = lastVolt(frame);
+					if(lv != null && frame - lv[0] < 25){
+						// anim: 20/100 s to move hand to new position, 75/100 s to move back
+						var animpos = frame - lv[0];
+						animpos = animpos <= 6? animpos/6 : 1 - (animpos - 6)/(25 - 6);
+						// elma actually uses the current frame here, which seems weird
+						if(lv[1] != Boolean(reader.turn(lv[0]) >> 1 & 1))
+							animpos *= -1;
+						var at = Math.atan2(handy - shouldery, handx - shoulderx) + animpos*2*Math.PI/3;
+						handx = shoulderx + shoulder2hand*Math.cos(at);
+						handy = shouldery + shoulder2hand*Math.sin(at);
+					}
+
+					var prod =
+						(shoulder2hand + upper + lower)*
+						(shoulder2hand - upper + lower)*
+						(shoulder2hand + upper - lower)*
+						(-shoulder2hand + upper + lower);
+					var jointangle = 0;
+					if(prod >= 0){
+						var circumr = shoulder2hand*upper*lower/Math.sqrt(prod);
+						jointangle = Math.asin(lower/(2*circumr));
+					}else
+						upper = upper/(upper + lower)*shoulder2hand;
+
+					var s2hangle = Math.atan2(handy - shouldery, handx - shoulderx);
+					var jointx = shoulderx + upper*Math.cos(s2hangle - jointangle);
+					var jointy = shouldery + upper*Math.sin(s2hangle - jointangle);
+
+					// arm
+					skewimage(canv, lgr.q1up_arm, 2/48, 0.5, 15/180, -10/48, jointx, jointy, shoulderx, shouldery);
+					skewimage(canv, lgr.q1forarm, 2/48, 0.75, 3/48, 8/48, handx, handy, jointx, jointy);
 				canv.restore();
-					
-					
-
-				canv.beginPath();
-				canv.moveTo(-1, 0);
-				canv.lineTo(1, 0);
-				canv.moveTo(0, -1);
-				canv.lineTo(0, 1);
-				canv.strokeStyle = "#00ff00";
-				canv.lineWidth = 1/50;
-				canv.stroke();
-
-
 			canv.restore();
-
-
-
 			canv.restore();
 		}
 
