5c3458016d4ab84de4b021a25560f7383e99ec11
Max Zerzouri <maxdamantus@gmail.com>
2024-05-05 10:53:40 +1200

    gh-pages: clean up unused files

--- src/util/geom.ts
+++
@@ -1,7 +0,0 @@
-export function rectsOverlap(x1: number, y1: number, w1: number, h1: number, x2: number, y2: number, w2: number, h2: number){
-	return ( // parentheses required! ASI!
-		x1 + w1 >= x2 &&
-		y1 + h1 >= y2 &&
-		x2 + w2 >= x1 &&
-		y2 + h2 >= y1);
-}
